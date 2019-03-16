aef3814bbd08214e81b1042e035cb0cce0f71e86
DaNing ZHU <36796050+328472816@users.noreply.github.com>
2019-03-16 14:51:58 +0800

    Add files via upload

---
+++ TCPIP/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TCPIPHelper;
+
+namespace TCPIP
+{
+    public partial class Form1 : Form
+    {
+        UdpHelper Udphelper;
+        TCPHelper Tcphelper;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_bind_Click(object sender, EventArgs e)
+        {
+            if (btn_bind.Text.Equals("绑定"))
+            {
+                Udphelper = new UdpHelper();
+                Udphelper.Udpinit(udp_ip.Text, udp_port.Text);
+                Udphelper.UdpserverStart();
+                Udphelper.rcmsg += ShowMess;
+                btn_bind.Text = "解绑";
+            }
+            else
+            {            
+                btn_bind.Text = "绑定";
+                Udphelper.UdpserverDestory();
+                
+            }
+        }
+
+        private delegate void MessDelegate<T>(T obj, T obj1, T obj2);
+        private void ShowMess(string ip,string port, string msg)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new MessDelegate<string>(ShowMess), ip, port,msg);
+            }
+            else
+            {
+                ListViewItem item = new ListViewItem(new string[]
+                {
+                    ip + ":" + port,msg,DateTime.Now.ToString()
+                });
+
+                udp_lbmsg.Items.Insert(0, item);
+
+                int megnum = Convert.ToInt32(udbmsgnum.Text, 10) + 1;
+                udbmsgnum.Text = "" + megnum;
+            }
+
+        }
+
+        private void btn_udpsend_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tb_udpsend.Text.Trim()))
+                return;
+            Udphelper.UdpSend(tb_udpsip.Text, tb_udpsport.Text, tb_udpsend.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lb_ip.Text = UdpHelper.getIPAddress();
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            udp_lbmsg.Items.Clear();
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            if (btn_connect.Text.Equals("连接"))
+            {
+                try
+                {
+                    btn_connect.Text = "断开";
+                    Tcphelper = new TCPHelper();
+                    Tcphelper.TcpClientinit(tb_connect.Text, tb_port.Text);
+                    Tcphelper.TcpClientStart();
+                    Tcphelper.rcmsg += TcpShowMess;
+                }
+                catch (Exception ee)
+                {
+                    btn_connect.Text = "连接";
+                    MessageBox.Show(ee.ToString());
+                }
+            }
+            else
+            {
+                btn_connect.Text = "连接";
+                Tcphelper.TcpClientClose();
+            }
+        }
+
+        private void btn_tcpclear_Click(object sender, EventArgs e)
+        {
+            lb_tcprc.Items.Clear();
+        }
+
+        private void btn_tcpsend_Click(object sender, EventArgs e)
+        {
+            Tcphelper.send(tb_tcpsend.Text);
+        }
+
+        private delegate void TcpMessDelegate<T1,T2>(T1 obj, T2 obj1);
+        private void TcpShowMess(byte[] msg, int count)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new TcpMessDelegate<byte[],int>(TcpShowMess), msg, count);
+            }
+            else
+            {
+                ListViewItem item = new ListViewItem(new string[]
+                {
+                     Encoding.UTF8.GetString(msg, 0, count),DateTime.Now.ToString()
+                });
+
+                lb_tcprc.Items.Insert(0, item);
+
+                int megnum = Convert.ToInt32(lb_num.Text, 10) + 1;
+                lb_num.Text = "" + megnum;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(Udphelper!=null)
+            Udphelper.UdpserverDestory();
+            if(Tcphelper!=null)
+            Tcphelper.TcpClientClose();
+        }
+    }
+}
---
+++ TCPIP/UdpHelper.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPIPHelper
+{
+    class UdpHelper
+    {
+        private UdpClient udpClient;
+        private IPEndPoint locatePoint;
+        private string remoteip;
+        private string port;
+        private bool backthrunning = true;
+
+
+        public void Udpinit(string ip, string port)
+        {
+            remoteip = ip;
+            this.port = port;
+            IPAddress locateIp = IPAddress.Parse(ip);
+            locatePoint = new IPEndPoint(locateIp, Convert.ToInt32(port));
+            udpClient = new UdpClient(locatePoint);
+            // udpClient.Connect(ip, Convert.ToInt32(port));
+            Socket uSocket = udpClient.Client;
+            uSocket.SetSocketOption(SocketOptionLevel.Socket,
+                 SocketOptionName.Broadcast, 1);
+        }
+
+        public void UdpserverStart()
+        {
+            Thread th = new Thread(UdpReceive);
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        public void UdpserverDestory()
+        {
+            backthrunning = false;
+            try
+            {
+                if(udpClient!=null)
+                udpClient.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public void UdpSend(string ip, string port, string msg)
+        {
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(msg.Trim());
+            IPAddress remoteIp = IPAddress.Parse(ip);
+            IPEndPoint remotePoint = new IPEndPoint(remoteIp, Convert.ToInt32(port));
+            udpClient.Send(buffer, buffer.Length, remotePoint);
+        }
+
+        public void UdpSend(string ip, string port, byte[] msg)
+        {
+            //byte[] buffer = System.Text.Encoding.UTF8.GetBytes(msg.Trim());
+            IPAddress remoteIp = IPAddress.Parse(ip);
+            IPEndPoint remotePoint = new IPEndPoint(remoteIp, Convert.ToInt32(port));
+            udpClient.Send(msg, msg.Length, remotePoint);
+        }
+
+        //udp接收委托
+        public delegate void Delegatercmsg(string ip, string port, string msg);
+        public Delegatercmsg rcmsg;
+
+        private void UdpReceive()
+        {
+
+            byte[] recBuffer;
+            //远端IP
+            //IPEndPoint remotePoint = new IPEndPoint(IPAddress.Any, port);
+            while (backthrunning)
+            {
+                try
+                {
+
+                    recBuffer = udpClient.Receive(ref locatePoint);
+                    if (recBuffer != null)
+                    {
+                        string str = Encoding.UTF8.GetString(recBuffer, 0, recBuffer.Length);
+                        rcmsg(remoteip, port, str);
+                    }
+                }
+                catch (Exception e)
+                {
+                    //   MessageBox.Show(e.ToString());
+                }
+            }
+
+
+        }
+
+
+
+        static public string getIPAddress()
+        {
+
+            //获取本地所有IP地址
+            IPHostEntry ipe = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress[] ip = ipe.AddressList;
+            for (int i = 0; i < ip.Length; i++)
+            {
+                if (ip[i].AddressFamily.ToString().Equals("InterNetwork"))
+                {
+
+                    return ip[i].ToString();
+                }
+            }
+            return null;
+        }
+    }
+
+    class TCPHelper
+    {
+        private TcpClient tcpClient;
+        private string remoteip;
+        private string port;
+        private Thread th;
+        NetworkStream stream = null;
+        private bool backthrunning = true;
+        byte[] recvData = new byte[1024 * 10];
+
+        public void TcpClientinit(string ip, string port)
+        {
+            remoteip = ip;
+            this.port = port;
+            tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Parse(ip), Convert.ToInt16(port, 10));
+            tcpClient.ReceiveBufferSize = 4096;
+            tcpClient.SendBufferSize = 4096;
+            stream = tcpClient.GetStream();
+        }
+
+        public void TcpClientClose()
+        {
+            backthrunning = false;
+            try
+            {
+                if(tcpClient!=null)
+                tcpClient.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void TcpClientStart()
+        {
+            backthrunning = true;
+            th = new Thread(recv);
+            th.Start();
+
+        }
+
+        public bool send(string msg)
+        {
+            byte[] outBound = Encoding.UTF8.GetBytes(msg);
+            stream.Write(outBound, 0, outBound.Length);
+            stream.Flush();
+            return true;
+        }
+
+        public bool send(byte[] msg)
+        {
+            stream.Write(msg, 0, msg.Length);
+            stream.Flush();
+            return true;
+        }
+
+        public delegate void Delegatercmsg(byte[] msg, int count);
+        public Delegatercmsg rcmsg;
+        private void recv()
+        {
+            while (backthrunning)
+            {
+                try
+                {
+                    int bufSize = tcpClient.ReceiveBufferSize;
+                    int count = stream.Read(recvData, 0, bufSize);
+                    rcmsg(recvData, count);
+                    // string str = Encoding.ASCII.GetString(recvData, 0, count);
+                    // Console.WriteLine(str);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
